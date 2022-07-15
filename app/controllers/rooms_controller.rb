0f9483104b5e890841f1f14c10300165d6006b61
KoshmareG <koshmareg@yandex.ru>
2022-07-15 19:59:47 +0700

    Chenge rooms controller for create different room types

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -26,24 +26,49 @@ class RoomsController < ApplicationController
     end
 
     def create
-        room = Room.new
-        room.name = ''
-        room.last_message = 'Начните диалог'
-        room.save
+        #Room_status specifies what room type need to create
+        #Room_status 0 - create new private room; 1 - create new common room; 2 - add new user to common room
+        
+        if params[:room_status].present? && (params[:room_status].to_i == 0 or params[:room_status].to_i == 1)
 
-        user_to_room = UserToRoom.new
-        user_to_room.user_id = current_user.id
-        user_to_room.room_id = room.id
+            room = Room.new
+            invite_user = User.find(params[:user_id])
 
-        invite_user = User.find(params[:user_id])
-        invite_user_to_room = UserToRoom.new
-        invite_user_to_room.user_id = invite_user.id
-        invite_user_to_room.room_id = room.id
+            if params[:room_status].to_i == 0
+                room.name = 'privat_room_username'
+            elsif params[:room_status].to_i == 1
+                room.name = 'Новая беседа'
+            end
 
-        user_to_room.save
-        invite_user_to_room.save
+            room.last_message = 'Сообщений пока нет'
+            room.room_status = params[:room_status].to_i
+            room.save
 
-        redirect_back(fallback_location: rooms_path)
+            user_to_room = UserToRoom.new
+            user_to_room.user_id = current_user.id
+            user_to_room.room_id = room.id
+
+            invite_user_to_room = UserToRoom.new
+            invite_user_to_room.user_id = invite_user.id
+            invite_user_to_room.room_id = room.id
+
+            user_to_room.save
+            invite_user_to_room.save
+
+        elsif params[:room_status].present? && params[:room_status].to_i == 2
+
+            room = Room.find(params[:room_id])
+            invite_user = User.find(params[:user_id])
+
+            invite_user_to_room = UserToRoom.new
+            invite_user_to_room.user_id = invite_user.id
+            invite_user_to_room.room_id = room.id
+
+            invite_user_to_room.save
+
+        end
+
+        redirect_to room_path(room)
     end
 
 end
