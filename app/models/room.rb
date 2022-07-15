175218924a53891f0fa03e898407adf7add32565
KoshmareG <koshmareg@yandex.ru>
2022-07-15 20:00:33 +0700

    Change room model for add room image

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,4 +4,9 @@ class Room < ApplicationRecord
     has_many :user_to_rooms, dependent: :delete_all
     has_many :messages, dependent: :delete_all
 
+    has_one_attached :image
+
+    validates :image, file_size: { less_than_or_equal_to: 5.megabytes },
+                file_content_type: { allow: ['image/jpeg', 'image/png', 'image/webp'] }
+
 end
