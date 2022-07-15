7bf3f2e2dd6a0f38cd21f8f24a4d68f30ccfea8c
KoshmareG <koshmareg@yandex.ru>
2022-07-15 11:42:53 +0700

    Minor update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  has_one :room, dependent: :destroy
-  has_one :user_to_rooms
+  has_one :room
+  has_one :user_to_rooms, dependent: :destroy
   has_many :messages
 
   has_one_attached :avatar
