66519b0cf90e58dd28f4c027aa252dd156687e9f
KoshmareG <koshmareg@yandex.ru>
2022-07-17 13:46:03 +0700

    Change display room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -22,14 +22,6 @@ class RoomsController < ApplicationController
     end
 
     def show
-        @users = User.where.not(id: current_user.id)
-        
-        user_rooms = []
-        all_user_rooms = UserToRoom.where(user_id: current_user.id)
-        all_user_rooms.each { |room| user_rooms << room.room_id }
-
-        @rooms = Room.find(user_rooms)
-
         @room = Room.find(params[:id])
 
         @messages = Message.where(room_id: params[:id])
