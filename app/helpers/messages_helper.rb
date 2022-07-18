cdefa8c4c67d8075d7d2ded9b438b52d519f0beb
KoshmareG <koshmareg@yandex.ru>
2022-07-18 20:05:36 +0700

    Add avatar on room header

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,11 @@
 module MessagesHelper
+
+    def display_massage_avatar message
+        if message.avatar.present?
+            image_tag(message.avatar, style: "width: 50px; height: 50px; object-fit: cover;", class: "user-avarat-style shadow")
+        else
+            image_tag("default_message_icon.png", size: "50x50", class: "user-avarat-style shadow")
+        end
+    end
+
 end
